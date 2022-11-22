cf892a3d577b5d028c8f7d4912079089d0bf3a43
NunaTna <tseganesh.abera@outlook.com>
2022-11-22 10:59:31 -0800

    Trial num

---
+++ print_num.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * print_number - A function that prints out numbers
+ * @n: Number to print
+ * Return: Num
+ */
+
+int print_number(int n)
+{
+	int sign, ten, chars;
+
+	chars = 0;
+	sign = 1;
+	ten = 1000000000;
+
+		if (n > 0)
+		{
+			n = n * -1;
+			sign = sign * -1;
+		}
+	if (n != 0)
+	{
+		while (n / ten == 0)
+		{
+			ten = ten / 10;
+		}
+		if (sign == 1)
+		{
+			_putchar('-');
+			chars++;
+		}
+		while (ten >= 1)
+		{
+			_putchar(-(n / ten) + '0');
+			chars++;
+			n = n % ten;
+			ten = ten / 10;
+		}
+	}
+	else
+	{
+		_putchar('0');
+		chars++;
+	}
+	return (chars);
+}
