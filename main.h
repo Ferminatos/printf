44a5a5f4689e48cf76e023bad0c218550d06804a
Bruke <BrukeD12@gmail.com>
2022-11-21 18:31:35 -0800

    printf one

---
+++ main.h
@@ -0,0 +1,34 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * struct vtype - struct vtype
+ * @tp: tp
+ * @f: function
+ */
+
+typedef struct vtype
+{
+
+	char tp;
+
+	void (*f)();
+
+} vtype_t;
+
+int _printf(const char *format, ...);
+
+void _write_buffer(char *buffer, int *index);
+
+void reset_buffer(char buffer[]);
+
+void format_s(va_list valist, char *buffer, int *index);
+
+void format_c(va_list valist, char *buffer, int *index);
+
+void format_perc(va_list valist, char *buffer, int *index);
+
+#endif
