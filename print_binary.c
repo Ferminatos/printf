ec4e92e0ee42032fd62de836712f2ace395ea135
Bruke <BrukeD12@gmail.com>
2022-11-25 15:50:47 -0800

    final fix

--- print_binary.c
+++ print_binary.c
@@ -9,7 +9,7 @@
 
 int print_bin(va_list args, params_t *params)
 {
-	unsigned int n = va_arg(ap, unsigned int);
+	unsigned int n = va_arg(args, unsigned int);
 	char *str = convert(n, 2, CONVERT_UNSIGNED, params);
 	int c = 0;
 
